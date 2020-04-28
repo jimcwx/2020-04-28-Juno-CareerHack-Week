3e2f687bfe43811703b3c1ff749c98ac4000797d
Jim Wang <jimcwx@gmail.com>
2020-04-28 12:20:19 -0400

    fetched data from server???

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+// registering the static "middleware"
+
+const staticMiddleware = express.static('public');
+app.use(staticMiddleware);
+
+// GET / will return "Hello World" in my browser
+// I can visit it by going to localhost:3000
+
+// i changed my route because I want my "index.html" to be from the server
+
+
+app.get('/api/json', (req, res) => {
+  const json = {};
+  json.message = "Hello World";
+  json.otherMessage = "Foo Baz Bar"
+  res.json(json);
 });
 
-app.listen(3000, () => console.log(`Now listening on localhost:3000`));
+// start the server and take port 3000
+app.listen(
+  // I am now actually running the server on port 3000;
+  
+  3000, 
+  // callback that runs when the server successfully starts
+  () => console.log(`Now listening on localhost:3000`)
+);
